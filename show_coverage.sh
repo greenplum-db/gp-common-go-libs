03414c4d3090632fef6c3c1a1fa05991c7e1112f
Jamie McAtamney <jmcatamney@pivotal.io>
2018-02-05 12:02:39 -0800

    Improve output of make coverage.
    Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>
    Co-authored-by: Chris Hajas <chajas@pivotal.io>

--- show_coverage.sh
+++ show_coverage.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+DIR="github.com/greenplum-db/gp-common-go-libs"
 RESULTS="/tmp/results.out"
-go test -coverprofile=/tmp/coverage.out 2> /dev/null | awk '{print $2 " test " $4 "\t" $5}' | awk -F"/" '{print $4}' > $RESULTS
+for PACKAGE in "gplog" "structmatcher"; do
+  # Generate code coverage statistics for all packages, write the coverage statistics to a file, and print the coverage percentage to the shell
+  go test -coverpkg "$DIR/$PACKAGE" "$DIR/$PACKAGE" -coverprofile="/tmp/unit_$PACKAGE.out" | awk '{printf("%s unit test coverage|%s", $2, $5)}' | awk -F"/" '{print $4}' >> $RESULTS
+  # Filter out the first "mode: set" line from each coverage file and concatenate them all
+  cat "/tmp/unit_$PACKAGE.out" | awk '{if($1!="mode:") {print $1 " " $2 " " $3}}' >> /tmp/coverage.out
+done
 
 # Print the total coverage percentage and generate a coverage HTML page
-go tool cover -func=/tmp/coverage.out | awk '{if($1=="total:") {print $1 "\t\t\t\t" $3}}' >> $RESULTS
-cat $RESULTS
+go tool cover -func=/tmp/coverage.out | awk '{if($1=="total:") {printf("%s|%s\n", $1, $3)}}' >> $RESULTS
+column -s"|" -t $RESULTS
 rm $RESULTS
 exit 0
