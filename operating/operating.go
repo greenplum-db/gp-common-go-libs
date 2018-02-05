c2c3bc71f6415837c42826a59082ec32b20e3b38
Chris Hajas <chajas@pivotal.io>
2018-02-05 12:03:42 -0800

    Add operating and testhelper packages
    The "operating" package contains SystemFunctions and related structs and
    functions from utils.  The "testhelper" package contains helper
    functions from testutils.
    
    Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>
    Co-authored-by: Chris Hajas <chajas@pivotal.io>

---
+++ operating/operating.go
@@ -0,0 +1,95 @@
+package operating
+
+/*
+ * This file contains structs and functions used as entry points for
+ * unit testing via dependency injection.
+ */
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+	"time"
+)
+
+var (
+	System = InitializeSystemFunctions()
+)
+
+/*
+ * Structs and functions for mocking out file reading
+ */
+
+type ReadCloserAt interface {
+	io.ReadCloser
+	io.ReaderAt
+}
+
+func OpenFileRead(name string, flag int, perm os.FileMode) (ReadCloserAt, error) {
+	var reader ReadCloserAt
+	var err error
+	reader, err = os.OpenFile(name, flag, perm)
+	return reader, err
+}
+
+func OpenFileWrite(name string, flag int, perm os.FileMode) (io.WriteCloser, error) {
+	var writer io.WriteCloser
+	var err error
+	writer, err = os.OpenFile(name, flag, perm)
+	return writer, err
+}
+
+/*
+ * SystemFunctions holds function pointers for built-in functions that will need
+ * to be mocked out for unit testing.  All built-in functions manipulating the
+ * filesystem, shell, or environment should ideally be called through a function
+ * pointer in System (the global SystemFunctions variable) instead of being called
+ * directly.
+ *
+ * All function pointers in SystemFunctions refer directly to built-in functions
+ * except for OpenFileRead and OpenFileWrite, which both refer to os.OpenFile but
+ * return either an io.ReadCloser or io.WriteCloser instead of an *os.File, to make
+ * mocking file opening in tests easier.
+ */
+
+type SystemFunctions struct {
+	Chmod         func(name string, mode os.FileMode) error
+	CurrentUser   func() (*user.User, error)
+	Getenv        func(key string) string
+	Getpid        func() int
+	Glob          func(pattern string) (matches []string, err error)
+	Hostname      func() (string, error)
+	IsNotExist    func(err error) bool
+	MkdirAll      func(path string, perm os.FileMode) error
+	Now           func() time.Time
+	OpenFileRead  func(name string, flag int, perm os.FileMode) (ReadCloserAt, error)
+	OpenFileWrite func(name string, flag int, perm os.FileMode) (io.WriteCloser, error)
+	ReadFile      func(filename string) ([]byte, error)
+	Stat          func(name string) (os.FileInfo, error)
+	Stdin         ReadCloserAt
+	Stdout        io.WriteCloser
+	Local         *time.Location
+}
+
+func InitializeSystemFunctions() *SystemFunctions {
+	return &SystemFunctions{
+		Chmod:         os.Chmod,
+		CurrentUser:   user.Current,
+		Getenv:        os.Getenv,
+		Getpid:        os.Getpid,
+		Glob:          filepath.Glob,
+		Hostname:      os.Hostname,
+		IsNotExist:    os.IsNotExist,
+		MkdirAll:      os.MkdirAll,
+		Now:           time.Now,
+		OpenFileRead:  OpenFileRead,
+		OpenFileWrite: OpenFileWrite,
+		ReadFile:      ioutil.ReadFile,
+		Stat:          os.Stat,
+		Stdin:         os.Stdin,
+		Stdout:        os.Stdout,
+		Local:         time.Local,
+	}
+}
---
+++ testhelper/functions.go
@@ -0,0 +1,40 @@
+package testhelper
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/greenplum-db/gp-common-go-libs/gplog"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+)
+
+func SetupTestLogger() (*gplog.Logger, *gbytes.Buffer, *gbytes.Buffer, *gbytes.Buffer) {
+	testStdout := gbytes.NewBuffer()
+	testStderr := gbytes.NewBuffer()
+	testLogfile := gbytes.NewBuffer()
+	testLogger := gplog.NewLogger(testStdout, testStderr, testLogfile, "gbytes.Buffer", gplog.LOGINFO, "testProgram")
+	gplog.SetLogger(testLogger)
+	return testLogger, testStdout, testStderr, testLogfile
+}
+
+func ExpectRegexp(buffer *gbytes.Buffer, testStr string) {
+	Expect(buffer).Should(gbytes.Say(regexp.QuoteMeta(testStr)))
+}
+
+func NotExpectRegexp(buffer *gbytes.Buffer, testStr string) {
+	Expect(buffer).ShouldNot(gbytes.Say(regexp.QuoteMeta(testStr)))
+}
+
+func ShouldPanicWithMessage(message string) {
+	if r := recover(); r != nil {
+		errorMessage := strings.TrimSpace(fmt.Sprintf("%v", r))
+		if !strings.Contains(errorMessage, message) {
+			Fail(fmt.Sprintf("Expected panic message '%s', got '%s'", message, errorMessage))
+		}
+	} else {
+		Fail("Function did not panic as expected")
+	}
+}
